6918570488663eaa2e7c239c36e1262dfafc25f6
Srinivas <srinivaskrishna124@gmail.com>
2023-04-14 12:51:39 +0530

    file changed

--- sum.java
+++ sum.java
@@ -3,15 +3,16 @@ public class SumOfNumbers2
 {  
 public static void main(String args[])  
 {  
-int x, y, sum;  
+int x, y, sum, z;
 Scanner sc = new Scanner(System.in);  
 System.out.print("Enter the first number: ");  
 x = sc.nextInt();  
 System.out.print("Enter the second number: ");  
-y = sc.nextInt();  
+y = sc.nextInt();
+z= sc.nextInt():
 sum = sum(x, y);  
-System.out.println("The sum of two numbers x and y is: " + sum);  
-}  
+System.out.println("The sum of two number x and y is: " + sum);  
+} 
 //method that calculates the sum  
 public static int sum(int a, int b)  
 {  
